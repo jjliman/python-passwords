59b2af714e7d8139ce891f02629300ca1fe73c9a
Justin Liman <justinliman97@gmail.com>
2021-03-18 18:12:54 -0700

    password sha1 encoding

--- checkmypass.py
+++ checkmypass.py
@@ -1,4 +1,5 @@
 import requests
+import hashlib
 
 
 def request_api_data(query_char):
@@ -8,7 +9,10 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res
 
-def pwned_api_check(password):
-    pass
 
-request_api_data('123')
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1password
+
+
+pwned_api_check('123')
