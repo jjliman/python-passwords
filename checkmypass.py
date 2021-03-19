658c4b5c8c2fb40fdd3aa8c333e7607c50f7deb5
Justin Liman <justinliman97@gmail.com>
2021-03-18 18:04:30 -0700

    request api data function

--- checkmypass.py
+++ checkmypass.py
@@ -1,1 +1,14 @@
 import requests
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+def pwned_api_check(password):
+    pass
+
+request_api_data('123')
